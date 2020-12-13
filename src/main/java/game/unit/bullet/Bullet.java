51e73b307173c8cb4d01725f6cf1c2fe1263a1f9
JiHoon <kjhktc@naver.com>
2020-12-13 22:10:11 +0900

    총알 레벨 추가

--- src/main/java/game/unit/bullet/Bullet.java
+++ src/main/java/game/unit/bullet/Bullet.java
@@ -8,30 +8,59 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 
 public abstract class Bullet extends Unit {
+    private final double direction;
+
+    /**
+     * 총알을 담고 있는 생성자
+     * 발사되는 총알 객체가 아님
+     *
+     * @param pos 총알이 생성될 위치
+     */
+    public Bullet(Point pos) {
+        super(pos, Unit.STAY);
+        this.direction = Unit.STAY;
+    }
+
+    /**
+     * 직접 발사되는 총알 객체
+     *
+     * @param pos       위치
+     * @param width     가로
+     * @param height    세로
+     * @param direction 방향
+     */
     public Bullet(Point pos, double width, double height, double direction) {
         super(new Point(pos.x - width / 2, pos.y), direction);
+        this.direction = direction;
     }
 
     @Override
     public boolean isCheckCollision() {
-        return true;
+        return direction != Unit.STAY;
     }
 
-    @Override
-    public void alive(Point pos) {
-        super.alive(new Point(pos.x - getWidth() / 2, pos.y));
+    public void alive(Point pos, int bulletLevel) {
+        super.alive(new Point(pos.x - (double) getWidth() / 2, pos.y));
     }
 
     @Override
     public void draw(Graphics2D g) {
+        if (direction == Unit.STAY) {
+            // 발사되는 총알 객체가 아니라는 의미
+            return;
+        }
         AffineTransform old = g.getTransform();
-        g.rotate(getDirection(), getX() + getWidth() / 2, getY() + getHeight() / 2);
+        g.rotate(getDirection(), pos.x + (double) getWidth() / 2, pos.y + (double) getHeight() / 2);
         super.draw(g);
         g.setTransform(old);
     }
 
     @Override
     public void move() {
+        if (direction == Unit.STAY) {
+            // 발사되는 총알 객체가 아니라는 의미
+            return;
+        }
         super.move();
         if (getX() < 0 || getX() > Stage.WIDTH) {
             kill();
@@ -39,5 +68,16 @@ public abstract class Bullet extends Unit {
         if (getY() < 0 || getY() > Stage.HEIGHT) {
             kill();
         }
+    }
+
+    protected double toDirection(double dgree) {
+        // 오른쪽이 0도 이기 때문에 해깔리지 않도록 아래 숫자들로 보정해줌.
+        // 북쪽이 0도로 맞춰줌
+        // 0 -> 270
+        // 90 -> 0
+        // 180 -> 90
+        // 270 -> 180
+        dgree = (dgree + 270) % 360;
+        return Math.toRadians(dgree);
     }
 }
--- src/main/java/game/unit/bullet/BulletFactory.java
+++ src/main/java/game/unit/bullet/BulletFactory.java
@@ -1,36 +1,31 @@
 package game.unit.bullet;
 
+import game.Point;
+import game.unit.bullet.guided.*;
+import game.unit.bullet.spread.*;
+import game.unit.bullet.straight.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import game.Point;
-import game.unit.bullet.guided.GuidedBullet;
-import game.unit.bullet.guided.GuidedLeftBullet;
-import game.unit.bullet.guided.GuidedRightBullet;
-import game.unit.bullet.spread.EnemySpreadBullet;
-import game.unit.bullet.spread.SpreadBullet;
-import game.unit.bullet.straight.EnemyStraightBullet;
-import game.unit.bullet.straight.StraightBullet;
 
 public class BulletFactory {
     private static final Logger logger = LoggerFactory.getLogger(BulletFactory.class);
 
-    public static Bullet getBullet(String bulletName, Point pos) throws Exception {
+    public static Bullet getBullet(String bulletName, Point pos, int bulletLevel) throws Exception {
         if (bulletName == null)
             return null;
 
         if (bulletName.equalsIgnoreCase("StraightBullet"))
-            return new StraightBullet(pos);
-        if (bulletName.equalsIgnoreCase("EnemyStraightBullet"))
-            return new EnemyStraightBullet(pos);
+            return new StraightBullet(pos, bulletLevel);
         if (bulletName.equalsIgnoreCase("SpreadBullet"))
-            return new SpreadBullet(pos);
-        if (bulletName.equalsIgnoreCase("GuidedLeftBullet"))
-            return new GuidedLeftBullet(pos);
-        if (bulletName.equalsIgnoreCase("GuidedRightBullet"))
-            return new GuidedRightBullet(pos);
+            return new SpreadBullet(pos, bulletLevel);
+        if (bulletName.equalsIgnoreCase("GuidedBullet"))
+            return new GuidedBullet(pos, bulletLevel);
+        if (bulletName.equalsIgnoreCase("EnemyStraightBullet"))
+            return new EnemyStraightBullet(pos, bulletLevel);
         if (bulletName.equalsIgnoreCase("EnemySpreadBullet"))
-            return new EnemySpreadBullet(pos);
+            return new EnemySpreadBullet(pos, bulletLevel);
+        if (bulletName.equalsIgnoreCase("EnemyGuidedBullet"))
+            return new EnemyGuidedBullet(pos, bulletLevel);
 
         logger.error("Can't find [" + bulletName + "] Bullet");
         return null;
@@ -42,14 +37,16 @@ public class BulletFactory {
 
         if (bulletName.equalsIgnoreCase("StraightBullet"))
             return StraightBullet.RATE;
-        if (bulletName.equalsIgnoreCase("EnemyStraightBullet"))
-            return EnemyStraightBullet.RATE;
         if (bulletName.equalsIgnoreCase("SpreadBullet"))
             return SpreadBullet.RATE;
-        if (bulletName.equalsIgnoreCase("GuidedLeftBullet") || bulletName.equalsIgnoreCase("GuidedRightBullet"))
+        if (bulletName.equalsIgnoreCase("GuidedBullet"))
             return GuidedBullet.RATE;
+        if (bulletName.equalsIgnoreCase("EnemyStraightBullet"))
+            return EnemyStraightBullet.RATE;
         if (bulletName.equalsIgnoreCase("EnemySpreadBullet"))
             return EnemySpreadBullet.RATE;
+        if (bulletName.equalsIgnoreCase("EnemyGuidedBullet"))
+            return EnemyGuidedBullet.RATE;
 
         logger.error("Can't find [" + bulletName + "] Bullet");
         return -1;
